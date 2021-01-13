f78e8cc40f90d52ab54f49aeb18d9432e7c641b3
nihnex <al_mg@yahoo.com.br>
2021-01-13 10:08:55 -0300

    atualizacao

--- fun_exercicio.cpp
+++ fun_exercicio.cpp
@@ -12,19 +12,38 @@ MODELBEGIN
 
 
 
-
-EQUATION("X")
+//Exemplo aula 01
 /*
-Variável X
+EQUATION("X")
+//Variável X
+RESULT(VL("X",1)+V("c"))
 */
 
+/*
+//Exercicio 01 - passeio aleatorio
+EQUATION("X")
+RESULT(VL("X",1)+RND)
+*/
+
+
+//Exercicio 01 - passeio aleatorio opcao 2
+EQUATION("X")
 RESULT(VL("X",1)+V("c"))
+EQUATION("c")
+v[0] = V("min");
+v[1] = V("max");
+v[2] = uniform(v[0], v[1]);
+RESULT(v[2])
 
 
-
-
-
-
+/*
+//Exercicio 01 - passeio aleatorio opcao 3
+EQUATION("X")
+v[0] = CURRENT;
+v[1] = V("c");
+v[2] = v[0]+v[1];
+RESULT(v[2])
+*/
 
 MODELEND
 
